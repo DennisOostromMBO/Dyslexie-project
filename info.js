f1200bfcf9c01a41d66a620790b1a1d54ff9fbbe
Dennis Oostrom <336914@student.mboutrecht.nl>
2024-02-14 16:34:58 +0100

    maximum toegevoegd

--- info.js
+++ info.js
@@ -1,12 +1,19 @@
+var huidigeGrootte = 16; // de standaard lettergrootte in pixels
+var minGrootte = 6; // de minimale lettergrootte in pixels
+var maxGrootte = 26; // de maximale lettergrootte in pixels
+
 function vergrootLettertype() {
-    var bodyElement = document.querySelector("body");
-    var huidigeGrootte = window.getComputedStyle(bodyElement, null).getPropertyValue("font-size");
-    var nieuweGrootte = parseInt(huidigeGrootte) + 2;
-    bodyElement.style.fontSize = nieuweGrootte + "px";
+  var bodyElement = document.querySelector("body");
+  if (huidigeGrootte < maxGrootte) { // controleer of de huidige grootte kleiner is dan het maximum
+    huidigeGrootte += 2; // verhoog de huidige grootte met 2
+    bodyElement.style.fontSize = huidigeGrootte + "px"; // pas de stijl aan
+  }
 }
 
 function verkleinLettertype() {
-    var bodyElement = document.querySelector("body");
-    var huidigeGrootte = window.getComputedStyle(bodyElement, null).getPropertyValue("font-size");
-    var nieuweGrootte = parseInt(huidigeGrootte) - 2;
-    bodyElement.style.fontSize = nieuweGrootte + "px";}
+  var bodyElement = document.querySelector("body");
+  if (huidigeGrootte > minGrootte) { // controleer of de huidige grootte groter is dan het minimum
+    huidigeGrootte -= 2; // verlaag de huidige grootte met 2
+    bodyElement.style.fontSize = huidigeGrootte + "px"; // pas de stijl aan
+  }
+}
